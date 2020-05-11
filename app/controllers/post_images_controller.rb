687dced7c321886bc05eae40e950facbbc4edeab
geruharuto <geruharuto2121@yahoo.co.jp>
2020-05-11 21:22:51 +0900

    2章途中

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,16 @@
+class PostImagesController < ApplicationController
+	def  new
+	end
+	def create
+		@post_image = PostImage.new(post_image_params)
+		@post_image.user_id = current_user.user_id
+		@post_image.save
+        redirect_to post_images_path
+	end
+	def index
+	    @post_images = PostImage.all
+	end
+	def show
+		@post_image = PostImage.find(params[:id])
+	end
+end
